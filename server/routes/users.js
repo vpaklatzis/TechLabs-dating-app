caa404189d4087f3662f79b0f1d76becb919d7c6
Vangelis <vpaklatzis@gmail.com>
2021-10-10 17:40:45 +0300

    Added delete user endpoint

--- server/routes/users.js
+++ server/routes/users.js
@@ -30,6 +30,19 @@ router.put("/:id", async (req, res) => {
 })
 
 // Delete user
+router.delete("/:id", async (req, res) => {
+    if(req.body.userId === req.params.id) {
+        try {
+            await User.findByIdAndDelete(req.params.id)
+            res.status(200).json("User has been deleted")
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    } else {
+        res.status(403).json("Unauthorized access")
+    }
+})
+
 // Get a single user
 
 module.exports = router
